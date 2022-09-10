35c2f21fdbf67710fe7b24ea806d5bb91562247b
Gerson Antonio <gelsonantonio53@outlook.com>
2022-09-09 21:24:02 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,14 +14,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_number = n % 10
+	last_number = n % 10;
 	if (last_number  >= 5)
-{
 	printf("Last digit of %d is %d and is greater than 5\n", n, last_number);
-}
 	else if (last_number == 0)
 	printf("Last digit of %d is %d and is 0\n", n, last_number);
-	else if (last_number < 6 && last_number !0)
+	else if (last_number < 6 && last_number != 0)
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n,
 	last_number);
 	return (0);
