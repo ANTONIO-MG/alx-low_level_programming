cf4ac21d45e23d2235caaef5a7f0600f468b87d4
Gerson Antonio <gelsonantonio53@outlook.com>
2022-09-13 19:45:32 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ *printing the alphabet - used simple for loop.
+ *
+ *Return: void because no return present
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c"\n");
+	}
+
+	/**_putchar('\n');*/
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+in this directory you will find all the tasks related to functions and nested loops in C programming, this functions tasl will perform various functionality.
