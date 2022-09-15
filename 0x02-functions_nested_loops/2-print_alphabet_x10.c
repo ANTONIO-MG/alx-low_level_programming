4418edc1fe8cce45a54b187d8992ebe948a5a7b9
Gerson Antonio <gelsonantonio53@outlook.com>
2022-09-14 21:01:18 -0700

    tasks 2-9 completed

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - Make alphabet x10 times
+  *
+  * Return: void
+  */
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int numerOfTimes = 0;
+
+	while (numerOfTimes <= 9)
+	{
+		for (alphabet; = 'a'; alphabet; <= 'z'; alphabet;++)
+		{
+			_putchar(alphabet;);
+		}
+
+		_putchar('\n');
+
+		numerOfTimes++;
+	}
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _isalpha - Checks for alphabetic character
+  * @c: The character to be checked
+  *
+  * Return: 1 for alphabetic character or 0 for anything else
+  */
+int _isalpha(int check)
+{
+	if ((check >= 65 && check <= 90) || (check >= 97 && check <= 122))
+	{
+		return (1);
+	}
+
+	return (0);
+}
