08d1f76d02e9fb0ce26e75511f2b2a0fb0ebfa09
Gerson Antonio <gelsonantonio53@outlook.com>
2022-09-15 18:33:03 -0700

    tasks 2 completed

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two digits
+ *@digit1: firts number
+ * @digit2: second number
+ * Return: returns the sum of two digits
+ */
+int mul(int digit1, int digit2)
+{
+	return (digit1 * digit2);
+
+}
