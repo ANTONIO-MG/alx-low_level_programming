2d0de16604f88dd0c15e142813f1e7c9aade8329
Gerson Antonio <gelsonantonio53@outlook.com>
2022-09-19 18:59:57 -0700

    tasks 0-7

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _strlen - returns the lenght of a string 
+  *
+  *
+  *return: 0
+  */
+
+int _strlen(char *s)
+{
+	s =char *s
+
+	strlen(s);
+
+	return (0);
+}
