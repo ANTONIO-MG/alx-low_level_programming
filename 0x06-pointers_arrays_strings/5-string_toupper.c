67bbe0e8b67f648e9662a338704a9a73cbe24466
Gerson Antonio <gelsonantonio53@outlook.com>
2022-09-22 16:57:28 -0700

    tasks 1-6

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * string_toupper - Changes all lowercase letters to uppercase
+  * @p: The string will be modified
+  * a@: empty var
+  * Return: char var
+  */
+char *string_toupper(char *p)
+{
+	int x = 0;
+
+	while (p[x])
+	{
+		if (p[x] >= 97 && p[x] <= 122)
+		{
+			p[x] -= 32;
+		}
+
+		x++;
+	}
+
+	return (p);
+}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * cap_string - ...
+  * @s: ...
+  * a@: epty var
+  *
+  * Return: char value
+  */
+char *cap_string(char *s)
+{
+	int x = 0, i;
+	int cspc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[x])
+	{
+		i = 0;
+
+		while (i < cspc)
+		{
+			if ((x == 0 || s[x - 1] == spc[i]) && (s[x] >= 97 && s[x] <= 122))
+				s[x] -= 32;
+
+			i++;
+		}
+
+		x++;
+	}
+
+	return (s);
+}
