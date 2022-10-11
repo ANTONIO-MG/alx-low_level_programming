749ab1500bb72c32b450c53079426fc5e003322a
Gerson <gerson@DESKTOP-TJCQE43.localdomain>
2022-10-11 13:14:49 +0200

    completed task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+~~~
+this repo has all the projects related to C programming structures and the Typedef where you create user defined types
+~~~
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+  * dog - this is the name of the struct holdinga all the information about the dog. 
+  *
+  * @name: the name of the dog
+  * @age: the age of the dog
+  * @owner: the owner of the dog.
+  *
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
