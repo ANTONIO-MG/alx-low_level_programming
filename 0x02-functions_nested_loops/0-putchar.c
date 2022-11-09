40e089422a4d558c1e2760fe7a869db2d222d5ac
Gerson Antonio <gelsonantonio53@outlook.com>
2022-11-09 09:35:14 -0800

    task 0 cor

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,24 @@
 #include "main.h"
 
 /**
- *  main - executes the _putchar function
- *  loop1@: counts the letters in the loop
- *  str_size@: size of string for iteration
- *  y@: string to be printed
- * return: 0
+* main - executes the _putchar function
+* loop1@: counts the letters in the loop
+* str_size@: size of string for iteration
+* y@: string to be printed
+* return: 0
 */
-int _putchar(char c);
 
-int main()
+int main(void)
 {
-    int loop1;
-    int str_size;
-    char y[9] = {'_','p','u','t','c','h','a','r'};
+	int loop1;
+	int str_size;
+	char y[9] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 
-    str_size = sizeof(y);
-    for (loop1 = 0; loop1 < str_size; loop1++)
-    {
-        _putchar(y[loop1]);
-    }
-    
-    return (0);
+	str_size = sizeof(y);
+	for (loop1 = 0; loop1 < str_size; loop1++)
+	{
+		putchar(y[loop1]);
+	}
 
+	return (0);
 }
