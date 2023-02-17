4b01f4fcf3aae0f373f9f05a5c3e54ddf791660d
Gerson Antonio <gelsonantonio53@outlook.com>
2023-02-17 13:55:18 -0800

    maid corrections to main.h and task 4 & 5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,42 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
-  * main - takes commandline arguments c and v
-  * description: this projects prints the first argument on STDOUT
-  * @argc: number of arguments  given
-  * @argv: arguments  given
-  *
-  * Return: returns 0 on sucees and error on failure
-  */
-
-int main(int argc, char **argv)
+ * main - Print result of adding given arguments
+ * @argc: Number of arguments
+ * @argv: Arguments recieved
+ *
+ * Return: 0 on success, 1 if theres a nondigit arg
+ */
+int main(int argc, char *argv[])
 {
+	int sum;
+	int count;
+	int i;
 
-	int sum, count;
-
+	count = 1;
 	sum = 0;
-
-	if (argc > 0)
-	{
-		for (count = 0; count < argc;)
-		{
-			sum += atoi(argv[count++]);
-		}
-			printf("%d\n", sum);
-
-	}
-	else if(argc < 0)
+	if (argc == 1)
 	{
 		printf("0\n");
+		return (0);
 	}
-
-	else
+	while (count < argc)
 	{
-		printf("Error\n");
-
-		return (1);
+		for (i = 0; argv[count][i] != '\0'; i++)
+		{
+			if (!(isdigit(argv[count][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[count]);
+		count++;
 	}
-
-	exit(EXIT_SUCCESS);
+	printf("%d\n", sum);
+	return (0);
 }
