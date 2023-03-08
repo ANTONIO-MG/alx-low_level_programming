b914bd2873c36630a82204c87b5bd0ec68722edb
Gerson Antonio <gelsonantonio53@outlook.com>
2023-03-08 13:05:50 -0800

    corrected task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prins out the given arg in reverse.
+ * _print_rev_recursion - prins out the given arg in reverse.
  * @s: the given string to be printed.
  *
  * Return: no return as is a void function.
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
