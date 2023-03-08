d615657a5eb234b95535f31b1255eb1622580841
Gerson Antonio <gelsonantonio53@outlook.com>
2023-03-08 10:45:48 -0800

    corrected task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _puts_recursion - prins out the given arg string using recursion.
  * @s: the given string to be printed.
- * 
+ *
  * Return: no return as is a void function.
  */
 
@@ -11,7 +11,6 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		_putchar('\n');
 	}
 
