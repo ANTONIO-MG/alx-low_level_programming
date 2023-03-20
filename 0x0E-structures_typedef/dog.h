dbaa7bdc65644b163d8f86f72a14762670970d49
Gerson Antonio <gelsonantonio53@outlook.com>
2023-03-20 16:59:55 -0700

    created the dog.h struct for the the dog information

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,19 @@
+#ifndef _DOG_STRUCT_
+#define _DOG_STRUCT_
+
+/**
+ * struct dog - the structure holding different properties
+ * @name: the name of the dog.
+ * @age: the age ogf of the dog.
+ * @owner: the owner of teh dog.
+ *
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+#endif /* _DOG_STRUCT_*/
