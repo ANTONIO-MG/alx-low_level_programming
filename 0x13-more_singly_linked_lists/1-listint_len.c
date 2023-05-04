a6e0dea16fe367480ffdfed33ba2bf37c8ae8369
Gerson Antonio <gelsonantonio@outlook.com>
2023-05-04 08:09:40 -0700

    corrected task 2

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_listint - function that prints all the elements of a listint_t list
+ * listint_len - function that prints all the elements of a listint_t list
  *
  * @h: the head node of the linked list.
  *
- * Return: returns the number of elements in a linked listint_t list
+ * Return: returns the number of elements in a linked listint_len
  */
 
 size_t listint_len(const listint_t *h)
