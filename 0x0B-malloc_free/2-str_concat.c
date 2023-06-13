eaa2f8ec443c3d795f096b90fb4f15bb2c41af4d
Gerson <gelsonantonio53@outlook.com>
2023-06-13 13:32:12 -0700

    corrected the 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,15 +11,23 @@ o * @s2: string to be concatenated to s1
 
 char *str_concat(char *s1, char *s2)
 {
-	int count1, count2, total_count = 0;
+	int count1 = 0, count2 = 0, total_count = 0;
 	char *new;
+	int sign_s1 = 0;
+	int sign_s2 = 0;
 
-	for (count1 = 0; count1 < (int)sizeof(s1); count1++)
-	;
-	for (count2 = 0; count2 < (int)sizeof(s2); count2++)
-	;
+	if (s1 == NULL)
+		s1 = "";
 
-	new = malloc((sizeof(char)) * (count1 + count2));
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[count1])
+		count1++;
+	while (s2[count2])
+		count2++;;
+
+	new = malloc((sizeof(char)) * (count1 + count2) + 2);
 
 	if (new == NULL)
 		return (NULL);
@@ -28,26 +36,18 @@ char *str_concat(char *s1, char *s2)
 
 	while (s1[count1])
 	{
-		if (s1 == NULL)
-		{
+		if (sign_s1 == 1)
 			count1++;
-		}
 		else
-		{
 			new[total_count++] = s1[count1++];
-		}
 	}
 
 	while (s2[count2])
 	{
-		if (s2 == NULL)
-		{
+		if (sign_s2 == 1)
 			count2++;
-		}
 		else
-		{
 			new[total_count++] = s2[count2++];
-		}
 	}
 
 	return (new);
