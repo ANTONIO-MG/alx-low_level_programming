cf37c86cd83a277ca6ff57e7e95564b256077493
Gerson <gelsonantonio53@outlook.com>
2023-06-13 13:40:26 -0700

    corrected the 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,19 +2,15 @@
 
 /**
  * str_concat - function that concatenates s2 to the end of s1.
- *  in memory, which contains a copy of the string given as a parameter.
  * @s1: the string that will be modified
-o * @s2: string to be concatenated to s1
- *
+ * @s2: string to be concatenated to s1
  * Return: NULL if it fails.
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int count1 = 0, count2 = 0, total_count = 0;
+	int count1 = 0, count2 = 0, total = 0, sign_s1 = 0, sign_s2 = 0;
 	char *new;
-	int sign_s1 = 0;
-	int sign_s2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -25,9 +21,9 @@ char *str_concat(char *s1, char *s2)
 	while (s1[count1])
 		count1++;
 	while (s2[count2])
-		count2++;;
+		count2++;
 
-	new = malloc((sizeof(char)) * (count1 + count2) + 2);
+	new = malloc((sizeof(char)) * (count1 + count2));
 
 	if (new == NULL)
 		return (NULL);
@@ -39,7 +35,7 @@ char *str_concat(char *s1, char *s2)
 		if (sign_s1 == 1)
 			count1++;
 		else
-			new[total_count++] = s1[count1++];
+			new[total++] = s1[count1++];
 	}
 
 	while (s2[count2])
@@ -47,9 +43,8 @@ char *str_concat(char *s1, char *s2)
 		if (sign_s2 == 1)
 			count2++;
 		else
-			new[total_count++] = s2[count2++];
+			new[total++] = s2[count2++];
 	}
 
 	return (new);
-
 }
