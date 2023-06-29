af949c3252357205de8ccf611bd7fa54cbceff97
Gerson <gelsonantonio53@outlook.com>
2023-06-29 11:04:59 -0700

    completed the implementation of function that prints all the elements of the singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_list - prints the num of nodes and data of a singly linked list.
+ * @h: the head node of the list to be printed
+ * Return: the number of nodes the list has
+ */
+
+
+size_t print_list(const list_t *h)
+{
+	int nodes = 0;
+
+	if (h == NULL)
+	{
+		printf("ERROR, list is empty\n");
+	}
+
+
+	while (h != NULL)
+	{
+		nodes++;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+
+	return (nodes);
+}
