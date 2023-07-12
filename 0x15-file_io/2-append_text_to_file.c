baa9376d441bdb47b6f8817703c26fd4cb99f9fe
Gerson <gelsonantonio53@outlook.com>
2023-07-11 19:29:27 -0700

    made correction to task 1 and 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,38 +1,40 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * append_text_to_file - appends text to teh end of file
  * @filename: file to be executed
  * @text_content: string to be written on file
- *
  * Return: 1 on success and -1 on failure
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int x;
-	int c = 0;
 
 	if (!filename)
 		return (-1);
-
 	x = open(filename, O_WRONLY | O_APPEND);
 	if (x == -1)
 		return (-1);
-
 	while (text_content[c])
 		c++;
-
-	if (c == '\0')
-		return (-1);
-
 	if (text_content)
 	{
-		write(x, text_content, c);
+		if (write(fd, text_content, _strlen(text_content)) == -1)
+			return (-1);
 	}
-
 	close(x);
-
 	return (1);
+}
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+	return (c);
 }
