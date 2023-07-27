9098115fb256795dc9cac452868a070b3ec3dd44
Gerson <gelsonantonio53@outlook.com>
2023-07-27 08:23:14 -0700

    corrected the string lenght

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,29 +8,38 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int sum = 0, len = 0, count = 0;
-	int array[] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+	unsigned int len = 0, count = 0, sum = 0;
 
-	if (*b == '\0')
+	if (b == NULL)
 		return (0);
 
-	while (b[len] != '\0')
-	{
-		len++;
-	}
-
+	len = _strlen(b);
 	while (len--)
 	{
-
 		if (b[len] != 48 && b[len] != 49)
 			return (0);
 
-		if (b[len] == '1')
-		{
-			sum += array[count];
-		}
+		if (b[len] == 49)
+			sum += 1 << count;
+
 		count++;
 	}
 
 	return (sum);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
 }
