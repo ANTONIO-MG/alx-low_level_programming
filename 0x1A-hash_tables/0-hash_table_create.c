7e440495d0dc7e8b583bf66238135592c347699d
Gerson Antonio <gelsonantonio53@outlook.com>
2023-09-08 19:55:40 +0200

    corrected the malloc allocation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,11 +8,26 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-
-	hash_table_t *table = malloc(sizeof(hash_node_t) * size);
+	unsigned int count = 0;
+	hash_node_t **node = NULL;
+	hash_table_t *table = malloc(sizeof(hash_node_t *) * size);
 
 	if (table == NULL)
 		return (NULL);
+
+	node = malloc(sizeof(hash_node_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	while (node[count])
+	{
+		node[count] = NULL;
+		count++;
+	}
+
+	table->size = size;
+	table->array = node;
 
 	return (table);
 }
