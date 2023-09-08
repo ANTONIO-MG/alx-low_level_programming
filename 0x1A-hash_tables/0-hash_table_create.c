5d05fd2521be3cafaf72799db8f6ac942f17cf27
Gerson Antonio <gelsonantonio53@outlook.com>
2023-09-08 19:13:33 +0200

    corrected the documentation error on task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * struct hash_table_create - creates a hash table
+ * hash_table_create - creates a hash table
  * @size: The size of the array to be created
  * Return: the address of the newly allocated space in memory
  */
@@ -9,10 +9,10 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 
-        hash_table_t *table = malloc(sizeof(hash_node_t) * size);
+	hash_table_t *table = malloc(sizeof(hash_node_t) * size);
 
-        if (NULL == table)
-                return (NULL);
+	if (table == NULL)
+		return (NULL);
 
-        return (table);
+	return (table);
 }
