6a3499156650fa76877486c92738aa66aef87046
Gerson Antonio <gelsonantonio53@outlook.com>
2023-09-08 20:31:16 +0200

    corrected the inappropriate memory allocation on the array3

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,12 +9,12 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_node_t **node = NULL;
-	hash_table_t *table = malloc(sizeof(hash_node_t *) * size);
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 		return (NULL);
 
-	node = malloc(sizeof(hash_node_t));
+	node = malloc(sizeof(hash_node_t *) * size);
 
 	if (node == NULL)
 		return (NULL);
